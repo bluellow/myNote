a2785d74f1888fd0a0ea41ab83c34415d116cc19
lihong <1091853304@qq.com>
2017-03-30 10:03:13 +0800

    Signed-off-by: lihong <1091853304@qq.com>

---
+++ js/main.js
@@ -0,0 +1,62 @@
+/**
+ * Created by Administrator on 2017/3/27.
+ */
+$(document).ready(function(){
+
+    var accordionColorNameArr = [""];
+    //加载目录
+    $.getJSON("catalog.json",function(data){
+        // 初始化
+        var content = data.content;
+        var $accordion = $("#accordion");
+        var slideSpeed = 300;
+        $accordion.empty();
+
+        $.each(content,function(i,val){
+            var $item = $("<li></li>");
+            $item.appendTo($accordion).addClass("item");
+            $item.append("<div>"+val[0].name + "</div>");
+
+            var $submenu = $("<ul></ul>");
+            $submenu.appendTo($item).addClass("submenu");
+            for(var j = 1;j<val.length;j++){
+                $submenu.append("<li><a href='javascript:void(0);' data-src="+  val[j].url+ ">" +
+                   val[j].name +"</a></li>");
+                $submenu.eq()
+            }
+            $submenu.hide();
+        //
+        })
+    });
+
+
+    $("#accordion").on("click",".item > div",function(slideSpeed){
+       var self = $(this);
+        var $selfparent = self.parent();
+        $selfparent.children(".submenu").slideToggle(slideSpeed);
+        $selfparent.siblings(".active").children("ul").slideToggle(slideSpeed);
+        $selfparent.siblings(".active").toggleClass("active");
+        $selfparent.toggleClass("active");
+    });
+
+    function showexam(obj){
+        var href = $(obj).data("src");
+        var html = "<iframe id='frames' src='"+href+"'  style='position:bolo;width:100%;height:100%;top:0;left:0'/>";
+        document.getElementById("contentbox").innerHTML = html;
+    }
+
+
+    $("#accordion").on("click",".item  .submenu li a",function(slideSpeed){
+        showexam(this);
+    })
+
+
+    $("#fullscreenbtn").click(function(){
+        var toolbarWidth = parseInt($(".toolbar").css("width"))
+        var sliderWidth = parseInt($(".slider").css("width"))
+
+
+        $(".slider").animate({"marginLeft":-(toolbarWidth+sliderWidth) + "px"},"slow");
+
+    })
+});
